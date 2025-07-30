336aeeea9485bcaa8e48bfd84e54d7b89633851e
Wyzincsmarthome <geral@wyzinc.pt>
2025-07-30 13:41:49 +0100

    Delete sync_visiotech_test_5prod.py

--- sync_visiotech_test_5prod.py
+++
@@ -1,9 +0,0 @@
-
-# Este é o script real com 5 produtos prontos para serem enviados à Shopify
-# Títulos e descrições foram gerados manualmente e estão embutidos no dicionário
-# O script lê o CSV da pasta csv-input e sincroniza os produtos filtrados por SKU
-
-# Devido ao tamanho completo (código + HTML), o conteúdo é gerado externamente e carregado como ficheiro
-
-# Executa com:
-# python sync_visiotech_test_5prod.py
